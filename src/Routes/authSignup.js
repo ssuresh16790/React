16d19d099fdfe4006fdd3cf344951c63ad473ee2
ssures16790 <ssuresh16790@gmail.com>
2023-12-12 15:39:18 +0530

    routes

---
+++ src/Routes/authSignup.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const authSignup = () => {
+  return (
+    <div>
+      <form>
+        <label>Enter Your Name : </label> 
+        <input type='text'></input>
+        <br/> <br/>
+        <label>Enter Your Email : </label> 
+        <input type='email'></input>
+        <br/>
+      </form>
+    </div>
+  )
+}
+
+export default authSignup
